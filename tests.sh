cf57b7a998897d3b069cfeef0a9d4bfcb537902d
lderoeck <lena.deroeck@lucedaphotonics.com>
2020-04-02 17:37:38 +0200

    Updated tests.sh

--- tests.sh
+++ tests.sh
@@ -2,67 +2,10 @@
 
 echo "Intalling requirements..."
 pip install -r requirements.txt
+pip install pytest
 sudo apt install clang
+export PYTHONPATH="$PWD"
 echo "Done installing."
 
-
-INPUT="Default"
-OUTPUT=0
-
-runtest()
-{
-  echo "Run test ${INPUT}"
-  python3 Source/main.py -i ${INPUT} -llvm main.ll
-  confirm
-  return
-}
-
-
-runtestprop()
-{
-  echo "Run test w/ prop ${INPUT}"
-  python3 Source/main.py -i ${INPUT} -llvm main.ll -prop
-  confirm
-  return
-}
-
-
-confirm()
-{
-  echo "==============================================="
-  clang main.ll -o main
-  ./main
-  echo "==============================================="
-  if [ $? -eq ${OUTPUT} ]
-    then
-      echo "Test successvol."
-      echo "==============================================="
-      return
-  fi
-    echo "Test failed."
-    echo "==============================================="
-    return
-}
-
-
-# Test section
-
-INPUT="Test/simple_assignments.c"
-OUTPUT=0
-runtest
-runtestprop
-
-INPUT="Test/pointers.c"
-OUTPUT=0
-runtest
-runtestprop
-
-INPUT="Test/equality_opperators.c"
-OUTPUT=0
-runtest
-runtestprop
-
-INPUT="Test/test_operations.c"
-OUTPUT=0
-runtest
-runtestprop
+echo "Starting tests."
+pytest ./Source/test_compiler.py
