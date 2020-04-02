d757c18ec9736656e7e8fc1566f90e380cd40ec9
lderoeck <lena.deroeck@lucedaphotonics.com>
2020-04-02 14:15:55 +0200

    Updated test command

--- Source/Test.py
+++ Source/Test.py
@@ -2,26 +2,42 @@ import filecmp
 import os
 
 
-def run_command(input_file: str, output_file: str):
+def run_command(input_file: str, output_file: str, expected_result_file: str):
     print("__________no propagation__________")
-    os.system("python3 Source/main.py -i %s -llvm main.ll" % input_file)
-    os.system("clang main.ll -o main && %s < ./main" % output_file)
+    try:
+        os.system("python3 Source/main.py -i %s -llvm main.ll" % input_file)
+        os.system("clang main.ll -o main && ./main > %s" % output_file)
+        result = compare(output_file, expected_result_file)
+    except Exception:
+        result = False
+    if result:
+        print("TEST PASSED")
+    else:
+        print("TEST FAILED")
 
 
-def run_command_prop(input_file: str, output_file: str):
+def run_command_prop(input_file: str, output_file: str, expected_result_file: str):
     print("__________w/ propagation__________")
-    os.system("python3 Source/main.py -i %s -llvm main.ll -prop" % input_file)
-    os.system("clang main.ll -o main && %s < ./main" % output_file)
+    try:
+        os.system("python3 Source/main.py -i %s -llvm main.ll -prop" % input_file)
+        os.system("clang main.ll -o main && ./main > %s" % output_file)
+        result = compare(output_file, expected_result_file)
+    except Exception:
+        result = False
+    if result:
+        print("TEST PASSED")
+    else:
+        print("TEST FAILED")
 
 
 def compare(result_file: str, expected_result_file: str):
     return filecmp.cmp(result_file, expected_result_file)
 
 
-def run_test(input_file: str, output_file: str, output_file_prop: str, expected_result_file: str):
-    run_command(input_file, output_file)
-    run_command_prop(input_file, output_file_prop)
-    return compare(output_file, expected_result_file) and compare(output_file_prop, expected_result_file)
+def run_test(input_file: str, output_file: str, expected_result_file: str):
+    print("__________Starting test %s__________" % input_file)
+    run_command(input_file, output_file, expected_result_file)
+    run_command_prop(input_file, output_file, expected_result_file)
 
 
 def run_tests():
@@ -29,15 +45,7 @@ def run_tests():
     result_files = ["./Test/Deadline1_result.txt"]
 
     for i in range(len(test_files)):
-        print("__________Starting test %s__________" % test_files[i])
-        try:
-            result = run_test(test_files[i], "test.txt", "test_prop.txt", result_files[i])
-        except Exception:
-            result = False
-        if result:
-            print("TEST PASSED")
-        else:
-            print("TEST FAILED")
+        run_test(test_files[i], "test.txt", result_files[i])
 
 
 if __name__ == "__main__":
