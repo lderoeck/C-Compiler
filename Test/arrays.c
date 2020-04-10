7a9c25f1fc725fd313a5f4c483341cfa902ec1c1
oli4 <olivier.van.houtte.sonic@gmail.com>
2020-04-10 21:33:38 +0200

    Tested: array opp

--- Test/arrays.c
+++ Test/arrays.c
@@ -6,16 +6,16 @@ int main(){
     //int i[3];
     int j[3];
     int i = 0;
-    j[0 + i] = 1;
-    j[0]++;
-    ++j[0];
+    j[i] = 1;
+    j[i]++;
+    j[i] *= 2;
+    j[i] = j[0] - 3;
     printf("%d\n", j[0]);
 
     int k[6] = {0, 1, 2};
     printf("%d\n", k[2]);
 
     int z = 10;
-
     float r[] = {0, 1, 2+z};
     printf("%f\n", r[2]);
 
