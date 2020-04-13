a689cb42d17724679bfbd1ca59c1e172eef08560
oli4 <olivier.van.houtte.sonic@gmail.com>
2020-04-13 11:58:01 +0200

    Commented: continue.c

--- Test/continue.c
+++ Test/continue.c
@@ -3,39 +3,34 @@
 
 int main(){
 
+    // continue in while
     int j = 0;
     while(j < 4){
-
         if (j == 2){
             j ++;
             continue;
         }
-
         printf("%d", j);
         j ++;
     }
 
+    // continue in do-while
     j = 0;
     do{
         if (j == 2){
             j ++;
             continue;
         }
-
         printf("%d", j);
         j ++;
      }while(j < 4);
 
-
-
+    // continue in for
     for (int i = 0; i < 4; i ++){
-
         if (i == 2){
             continue;
         }
-
         printf("%d", i);
-
     }
 
     return 0;
